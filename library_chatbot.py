3531d42016b6453f1b97c633a8646976744b97fd
gyuseon362-jpg <gyuseon362@gmail.com>
2025-11-15 10:45:29 +0900

    Update library_chatbot.py

--- library_chatbot.py
+++ library_chatbot.py
@@ -81,7 +81,7 @@ def get_vectorstore(_docs):
 # PDF 문서 로드-벡터 DB 저장-검색기-히스토리 모두 합친 Chain 구축
 @st.cache_resource
 def initialize_components(selected_model):
-    file_path = r"/mount/src/librarychatbot_gemini/[챗봇프로그램및실습] 부경대학교 규정집.pdf"
+    file_path = r"/mount/src/librarychatbot_gemini/인천 섬 갯벌에 대한 생태적 가치화 방안과 적용.pdf"
     pages = load_and_split_pdf(file_path)
     vectorstore = get_vectorstore(pages)
     retriever = vectorstore.as_retriever()
@@ -131,7 +131,7 @@ def initialize_components(selected_model):
     return rag_chain
 
 # Streamlit UI
-st.header("국립부경대 도서관 규정 Q&A 챗봇 💬 📚")
+st.header("인천 섬 갯벌에 대한 생태적 가치화 방안과 적용 Q&A 챗봇 💬 📚")
 
 # 첫 실행 안내 메시지
 if not os.path.exists("./chroma_db"):
